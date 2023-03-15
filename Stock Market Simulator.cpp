86ee6104d5c40f704af1c505e7560399ba75e0a5
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-03-16 01:08:30 +0600

    Add files via upload

---
+++ Stock Market Simulator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+float a,p=10000,r=0.03;
+for(int day=1;day<=30;day++){
+a=p*pow(1+r,day);
+cout<<day<<":"<<a<<endl;
+}
+return 0;
+}
